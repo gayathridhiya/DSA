0af2b1dbd6c5517bebbd8efb787048fbef35f5e0
gayathridhiya <gayathridhiya1212@gmail.com>
2024-04-09 07:02:29 +0530

    largest unique substr

---
+++ imp_dsa_probs/Snow/uniqueSubstr.js
@@ -0,0 +1,42 @@
+let s="111345611298704";
+
+//11134563
+// 1: 3, 3: 2, 4 : 1, 5:1, 6: 1, 3:1
+
+//length = 4
+
+// 1 1 1 3 4 5 6 3
+// | |
+//   | |            if char already found so far => l=i,r=i+1, push str in dict
+//     | |          if not => r++ & add in dict
+//     |   |
+//     |       |    and r!=str.length
+
+
+function findUniqueSubstring(s){
+
+    let res = [];
+
+    if(s.length <= 1) return res; //edge case
+
+    let m = {};
+    let left = 0;
+    let right = left+1;
+
+    let i = 0;
+
+    while(i<s.length){
+        if(m[s[i]]){ //char already in m
+            left = i;
+            right = i+1;
+        }else{
+            m[s[i]] = 1;
+            res.push(s.substr(left, right-1));
+            right+=1;
+        }
+        i++;
+    }
+    return res;
+}
+
+console.log(findUniqueSubstring(s).sort((a,b) => b.length - a.length)[0])
