2be1c47702bf421d72bed5f59a05c8959f65368f
gayathridhiya <gayathridhiya1212@gmail.com>
2024-04-29 14:05:05 +0530

    WM Rating

--- React_machine_coding/star-rating/src/component/Star.jsx
+++ React_machine_coding/star-rating/src/component/Star.jsx
@@ -7,32 +7,40 @@ export default function Star() {
 
     console.log(clicked)
 
-    const handleSelection = e =>{
-        let value = e.target.getAttribute("value");
-        console.log(e.target.getAttribute("value"))
-        setClicked((prev) => {return {...prev, [value] : !prev[value]}})
-    }
+    // const handleSelection = e =>{
+    //     let value = e.target.getAttribute("value");
+    //     console.log(e.target.getAttribute("value"))
+    //     setClicked((prev) => {return {...prev, [value] : !prev[value]}})
+    // }
 
-    useEffect(() => {
-        let cnt = 0;
-        for(let i in clicked){
-            if(clicked[i]) cnt++;
-        }
-        setCount(cnt);
-    }, [clicked])
+    // useEffect(() => {
+    //     let cnt = 0;
+    //     for(let i in clicked){
+    //         if(clicked[i]) cnt++;
+    //     }
+    //     setCount(cnt);
+    // }, [clicked])
+
+    const handleSelection = (id) => {
+        setCount(id)
+    }
 
   return (
         <div className='starContainer'>
             <h2>Rate us</h2>
-            {   
-                <div className='starContent'>
-                    <div value ="1" className={clicked[1] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
-                    <div value ="2" className={clicked[2] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
-                    <div value ="3" className={clicked[3] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
-                    <div value ="4" className={clicked[4] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
-                    <div value ="5" className={clicked[5] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
-                </div>
-            }
+  
+            <div className='starContent'>
+
+                {
+                    Array.from({length : 5}).map( (curr, idx) => <div value={idx+1} onClick = {() => handleSelection(idx+1)}>⭐</div>)
+                }
+                {/* <div value ="1" className={clicked[1] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
+                <div value ="2" className={clicked[2] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
+                <div value ="3" className={clicked[3] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
+                <div value ="4" className={clicked[4] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div>
+                <div value ="5" className={clicked[5] ? "rated" : null} onClick = {(e) => handleSelection(e)}>⭐</div> */}
+            </div>
+
             <>you rated {count} stars</>
         </div>
   )
